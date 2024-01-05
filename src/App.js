ea30cd5837be5a3eb5143ff55320df67eeb887ef
sharmilidas33 <sharmilidas1@gmail.com>
2024-01-05 23:32:47 +0530

    Projects Section Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/NavBar/Navbar";
+import Intro from "./components/NavBar/Home/Intro";
+import Skills from "./components/About/Skills";
+import Projects from "./components/Projects/Projects";
+import Experience from "./components/Experience/Experience";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <Intro/>
+        <Skills/>
+        <Projects/>
+        <Experience/>
+        <Contact/>
+        <Footer/>
     </div>
   );
 }
