2a6751f05db491ab839be3903f8e0f7ab89ab0d3
sharmilidas33 <sharmilidas1@gmail.com>
2024-06-22 13:23:34 +0530

    Added see more

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,50 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './exp.css'
 
 const Experience = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  const experiences = [
+    {
+      title: 'PHP Developer Intern,',
+      company: 'at Onlighten Media, (Mumbai, Maharashtra, India)',
+      description: 'My job is to build web applications, by learning from the seniors in the company and tackling new challenges!'
+    },
+    {
+      title: 'Junior Software Developer Intern,',
+      company: 'at Hellbent Software & Educational Services LLP, (Vidisha, Madhya Pradesh, India)',
+      description: "I worked as a junior software developer in this company, where I had to implement codes according to the IDE & meet client's requirements on time. I also had to revamp and reuse codes, that had been used before in other significant projects. My work was based at Web Development and also included writing well-defined reports of the codes I implemented."
+    },
+    {
+      title: 'Formatting and Presentation Intern,',
+      company: 'at IIT Bombay, (Mumbai, Maharashtra, India)',
+      description: 'IIT Bombay has served as a great workspace where I learnt how team work is done and also how well communication skills matter, here I had to code in LaTex, present my work to seniors and report on time.'
+    },
+    {
+      title: 'Technical Content Writing Intern,',
+      company: 'at Nettv4u, (Chennai, Tamil Nadu, India)',
+      description: 'In this Internship, I had to write Articles on the new emerging programming languages, Artificial Intelligence, Blockchains, Deep Learning and more..'
+    },
+    {
+      title: 'Data Analytics Intern,',
+      company: 'at IBM, collaboration with AICTE.',
+      description: 'This was a project based internship where I acquired great knowledge on Python and also worked on a project which was about Analysing a Superstore Dataset, productively.'
+    }
+  ];
+
+  const displayedExperiences = showAll ? experiences : experiences.slice(0, 3);
+
   return (
     <div className="container" id="experience">
-    <section id='experience1'>
-            <h1 className='title'>Experience</h1>
-            <p className='titlePara'>I am an individual who believes that real time project experiences are a lot more valuable than daily lectures. I have done a few internships, and have learnt a lot from each one of them.</p>
+      <section id='experience1'>
+        <h1 className='title'>Experience</h1>
+        <p className='titlePara'>I am an individual who believes that real time project experiences are a lot more valuable than daily lectures. I have done a few internships, and have learnt a lot from each one of them.</p>
         <div className="contents">
-            <div className="box">
-                <h3>PHP Developer Intern,</h3>
-                <h4>at Onlighten Media, <br />(Mumbai, Maharashtra, India)</h4>
-                <p>My job is to build web applications, by learning from the seniors in the company and tackling new challenges!</p>
+          {displayedExperiences.map((exp, index) => (
+            <div className="box" key={index}>
+              <h3>{exp.title}</h3>
+              <h4>{exp.company}</h4>
+              <p>{exp.description}</p>
             </div>
+          ))}
         </div>
-        <div className="contents">
-            <div className="box">
-                <h3>Junior Software Developer Intern,</h3>
-                <h4>at Hellbent Software & Educational Services LLP, <br />(Vidisha, Madhya Pradesh, India)</h4>
-                <p>I worked as a junior software developer in this company, where I had to implement codes according to the IDE & meet client's requirements on time. I also had to revamp and reuse codes, that had been used before in other significant projects. My work was based at Web Development and also included writing well-defined reports of the codes I implemented.</p>
-            </div>
-        </div>
-        <div className="contents">
-            <div className="box">
-                <h3>Formatting and Presentation Intern,</h3>
-                <h4>at IIT Bombay,  <br />(Mumbai, Maharashtra, India)</h4>
-                <p>IIT Bombay has served as a great workspace where I learnt how team work is done and also how well communication skills matter, here I had to code in LaTex, present my work to seniors and report on time.</p>
-            </div>
-        </div>
-        <div className="contents">
-            <div className="box">
-                <h3>Technical Content Writing Intern,</h3>
-                <h4>at Nettv4u, <br />(Chennai, Tamil Nadu, India)</h4>
-                <p>In this Internship, I had to write Articles on the new emerging programming languages, Artifical Intelligence, Blockchains, Deep Learning and more..</p>
-            </div>
-        </div>
-        <div className="contents">
-            <div className="box">
-                <h3>Data Analytics Intern,</h3>
-                <h4>at IBM, collaboration with AICTE.</h4>
-                <p>This was a project based internship where I acquired great knowledge on Python and also worked on a project which was about Analysing a Superstore Dataset, productively.</p>
-            </div>
-        </div>
-    </section>
+        <button className="see-more" onClick={() => setShowAll(!showAll)}>
+          {showAll ? 'Show Less' : 'See More'}
+        </button>
+      </section>
     </div>
   )
 }
 
-export default Experience
+export default Experience;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,102 @@
-import React from 'react'
-import './projects.css'
-import project1 from '../../assets/project1.png'
-import newsapp from '../../assets/newsapp.png'
-import textanalyzer from '../../assets/textanalyzer.png'
-import numbergame from '../../assets/numbergame.png'
-import digitalclock from '../../assets/digitalclock.png'
-import jokegenerator from '../../assets/jokegenerator.png'
-import todo from '../../assets/Todo.png'
-import portfolio from '../../assets/portfolio.png'
-import ebook from '../../assets/ebook.png'
-import pepperSpray from '../../assets/pepperSpray.png'
-import discordBot from '../../assets/discordBot.png'
+import React, { useState } from 'react';
+import './projects.css';
+import project1 from '../../assets/project1.png';
+import newsapp from '../../assets/newsapp.png';
+import textanalyzer from '../../assets/textanalyzer.png';
+import numbergame from '../../assets/numbergame.png';
+import digitalclock from '../../assets/digitalclock.png';
+import jokegenerator from '../../assets/jokegenerator.png';
+import todo from '../../assets/Todo.png';
+import portfolio from '../../assets/portfolio.png';
+import ebook from '../../assets/ebook.png';
+import pepperSpray from '../../assets/pepperSpray.png';
+import discordBot from '../../assets/discordBot.png';
 
 const Projects = () => {
+    const [showAll, setShowAll] = useState(false);
+
+    const projects = [
+        {
+            img: pepperSpray,
+            alt: 'project0',
+            title: 'Pepper Spray',
+            description: 'A women safety application developed using React Native for recognising red zones and formulating a safe community forum.',
+            link: 'https://github.com/SohamB21/PepperSpray-App'
+        },
+        {
+            img: ebook,
+            alt: 'project1',
+            title: 'EBook using MERN',
+            description: 'A MERN Stack Application with user authentication, dynamic data fetching, managing & more..',
+            link: 'https://e-book-using-mern.vercel.app/'
+        },
+        {
+            img: discordBot,
+            alt: 'project1a',
+            title: "Sharmili's Discord Bot",
+            description: "Sharmili's Discord Bot brings Google's AI power to your server! Use @ commands to interact with the AI and enhance your server experience.",
+            link: 'https://github.com/sharmilidas33/Sharmili-s-Discord-Bot'
+        },
+        {
+            img: textanalyzer,
+            alt: 'project2',
+            title: 'Text Analyzer',
+            description: 'A React App for analyzing and discovering new things with texts!',
+            link: 'https://textanalyzer-by-sharmili.netlify.app/'
+        },
+        {
+            img: portfolio,
+            alt: 'project3',
+            title: 'My PortFolio.',
+            description: 'A portfolio website of mine, developed using React JS!',
+            link: 'https://sharmili-s-portfolio.vercel.app/'
+        },
+        {
+            img: newsapp,
+            alt: 'project4',
+            title: 'SnapNews!',
+            description: 'A news application developed using React JS.',
+            link: 'https://github.com/sharmilidas33/SnapNews-News-App-with-React'
+        },
+        {
+            img: project1,
+            alt: 'project5',
+            title: 'Cosmetics Landing Page',
+            description: 'A product landing page developed using Tailwind CSS & JS.',
+            link: 'https://cosmeticsproduct-webpage.netlify.app/'
+        },
+        {
+            img: numbergame,
+            alt: 'project6',
+            title: 'Number Guessing Game',
+            description: 'This has been developed using JavaScript.',
+            link: 'https://sharmilidas33.github.io/Number-Guessing-Game/'
+        },
+        {
+            img: digitalclock,
+            alt: 'project7',
+            title: 'Real Time Digital Clock',
+            description: 'Developed using Javascript mostly, rest is HTML & CSS.',
+            link: 'https://sharmilidas33.github.io/Real-time-digital-clock/'
+        },
+        {
+            img: jokegenerator,
+            alt: 'project8',
+            title: 'Tell me a joke!',
+            description: 'A programming joke generator developed using Bootstrap & JS!',
+            link: 'https://sharmilidas33.github.io/Tell-me-a-joke-/'
+        },
+        {
+            img: todo,
+            alt: 'project9',
+            title: 'Todo List',
+            description: 'A frontend based todo list developed using JS.',
+            link: 'https://sharmilidas33.github.io/To-Do-List-/'
+        },
+    ];
+
+    const visibleProjects = showAll ? projects : projects.slice(0, 4);
+
     return (
         <section id="project">
             <div className="container" id="projects">
@@ -21,98 +105,23 @@ const Projects = () => {
                     <p className='sub-para'>I like to create various projects using the skills I have. <br /> This portfolio is itself a project of mine developed using ReactJS. <br /> Here's some more -</p>
                 </div>
                 <div className="work-list">
-                    <div className="work">
-                        <img src={pepperSpray} alt="project0" />
-                        <div className="layer">
-                            <h3>Pepper Spray</h3>
-                            <p>A women safety application developed using React Native for recognising red zones and formulating a safe community forum.</p>
-                            <a href="https://github.com/SohamB21/PepperSpray-App">See More!</a>
+                    {visibleProjects.map((project, index) => (
+                        <div className="work" key={index}>
+                            <img src={project.img} alt={project.alt} />
+                            <div className="layer">
+                                <h3>{project.title}</h3>
+                                <p>{project.description}</p>
+                                <a href={project.link}>See More!</a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="work">
-                        <img src={ebook} alt="project1" />
-                        <div className="layer">
-                            <h3>EBook using MERN</h3>
-                            <p>A MERN Stack Application with user authentication, dynamic data fetching, managing & more..</p>
-                            <a href="https://e-book-using-mern.vercel.app/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={discordBot} alt="project1a" />
-                        <div className="layer">
-                            <h3>Sharmili's Discord Bot</h3>
-                            <p>Sharmili's Discord Bot brings Google's AI power to your server! Use @ commands to interact with the AI and enhance your server experience.</p>
-                            <a href="https://github.com/sharmilidas33/Sharmili-s-Discord-Bot">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={textanalyzer} alt="project2" />
-                        <div className="layer">
-                            <h3>Text Analyzer</h3>
-                            <p>A React App for analyzing and discovering new things with texts!</p>
-                            <a href="https://textanalyzer-by-sharmili.netlify.app/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={portfolio} alt="project3" />
-                        <div className="layer">
-                            <h3>My PortFolio.</h3>
-                            <p>A portfolio website of mine, developed using React JS!</p>
-                            <a href="https://sharmili-s-portfolio.vercel.app/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={newsapp} alt="project4" />
-                        <div className="layer">
-                            <h3>SnapNews!</h3>
-                            <p>A news application developed using React JS.</p>
-                            <a href="https://github.com/sharmilidas33/SnapNews-News-App-with-React">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={project1} alt="project5" />
-                        <div className="layer">
-                            <h3>Cosmetics Landing Page</h3>
-                            <p>A product landing page developed using Tailwind CSS & JS.</p>
-                            <a href="https://cosmeticsproduct-webpage.netlify.app/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={numbergame} alt="project6" />
-                        <div className="layer">
-                            <h3>Number Guessing Game</h3>
-                            <p>This has been developed using JavaScript.</p>
-                            <a href="https://sharmilidas33.github.io/Number-Guessing-Game/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={digitalclock} alt="project7" />
-                        <div className="layer">
-                            <h3>Real Time Digital Clock</h3>
-                            <p>Developed using Javascript mostly, rest is HTML & CSS.</p>
-                            <a href="https://sharmilidas33.github.io/Real-time-digital-clock/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={jokegenerator} alt="project8" />
-                        <div className="layer">
-                            <h3>Tell me a joke!</h3>
-                            <p>A programming joke generator developed using Bootstrap & JS!</p>
-                            <a href="https://sharmilidas33.github.io/Tell-me-a-joke-/">See More!</a>
-                        </div>
-                    </div>
-                    <div className="work">
-                        <img src={todo} alt="project9" />
-                        <div className="layer">
-                            <h3>Todo List</h3>
-                            <p>A frontend based todo list developed using JS.</p>
-                            <a href="https://sharmilidas33.github.io/To-Do-List-/">See More!</a>
-                        </div>
-                    </div>
+                    ))}
                 </div>
+                <button className="see-more" onClick={() => setShowAll(!showAll)}>
+                    {showAll ? 'Show Less' : 'See More'}
+                </button>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Projects
+export default Projects;
